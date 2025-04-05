9ea7c6a4ac50e2c4ffef9d8f104650bb19197730
Pranav Umesh Ghodke <pranavghodke626@gmail.com>
2025-04-05 15:20:12 +0530

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,20 +1,19 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCl7QkGoYV--CCak2oyT2TQrV63oAOO8iQ",
-  authDomain: "expense-tracker-30f0c.firebaseapp.com",
-  projectId: "expense-tracker-30f0c",
-  storageBucket: "expense-tracker-30f0c.appspot.com", // ✅ Fixed
-  messagingSenderId: "204002980483",
-  appId: "1:204002980483:web:ec9f11ea5f8bd12b18e1b6",
-  measurementId: "G-18PH3NK79G",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
 export default app;
